509c95e6c5b7c1b17182f80a7e6b2d21e7aaa852
LeE R <omlhllkyr@gmail.com>
2018-05-30 21:07:40 +0800

    fix conflict

--- app/src/main/java/johnleung/posms/MainActivity.java
+++ app/src/main/java/johnleung/posms/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
      */
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
