70182f0e433cd19fe3756359ad0618dd9cf7aac3
Unknown <funleung117@gmail.com>
2018-05-30 23:39:39 +0800

    Fix Conflict

--- app/src/main/java/johnleung/posms/LoginActivity.java
+++ app/src/main/java/johnleung/posms/LoginActivity.java
@@ -1,25 +1,27 @@
 package johnleung.posms;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private String TAG = "ACTION";
 
 
-//    Button btnLogin = (Button) findViewById(R.id.btnLogin);
+    Button btnLogin = (Button) findViewById(R.id.btnLogin);
 //    Button btnRegister = (Button) findViewById(R.id.btnRegister);
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_login);
 
         Log.d(TAG,"Entered Login");
+
+
 
     }
 }
