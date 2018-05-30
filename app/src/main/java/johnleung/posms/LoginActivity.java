d1db2a6a759d8a04bf1196e337b9c86759d3cde8
Unknown <funleung117@gmail.com>
2018-05-31 02:04:26 +0800

    Update Progress

--- app/src/main/java/johnleung/posms/LoginActivity.java
+++ app/src/main/java/johnleung/posms/LoginActivity.java
@@ -1,35 +1,63 @@
 package johnleung.posms;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private String TAG = "ACTION";
-    private boolean login = true;
+    private Button btnLogin, btnRegister;
+    private String uid, upwd, uname;
+//    private ViewPager mViewPager = MainActivity.mViewPager;
+    private String input_id, input_password;
 
+    private boolean login = true;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
-
         Log.d(TAG,"Entered Login");
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+//        Intent intent = getIntent();
+//        boolean noLogin = intent.getBooleanExtra("noLogin", true);
+
+//        login = !(!(noLogin));
+
+        SharedPreferences result = getSharedPreferences("personal", 0);
+        uid = result.getString("uid", "");
+        upwd = result.getString("upwd", "");
+        uname = result.getString("uname", "");
+
+        TextView loginid = findViewById(R.id.user_id);
+        TextView loginpwd = findViewById(R.id.user_pwd);
+
+        input_id = loginid.getText().toString();
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                if(login){
-                    Intent login = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(login);
+
+//                Log.d(TAG, "uid: " + uid);
+//                Log.d(TAG, "input_id: " + input_id);
+
+                if(true){
+//                if(uid.equals(input_id)){
+//                    Intent login = new Intent(LoginActivity.this, MainActivity.class);
+//                    startActivity(login);
+                    LoginActivity.super.onBackPressed();
+
                 }
             }
         });
--- app/src/main/java/johnleung/posms/RegisterActivity.java
+++ app/src/main/java/johnleung/posms/RegisterActivity.java
@@ -1,60 +1,60 @@
 package johnleung.posms;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     private static String TAG = "ACTION";
     Button reg, clear;
+    TextView uid, pwd, name;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Intent intent = getIntent();
 
         reg = (Button) findViewById(R.id.btnRegister);
         clear = (Button) findViewById(R.id.btnClear);
+        uid = (TextView) findViewById(R.id.uid);
+        pwd = (TextView) findViewById(R.id.pwd);
+        name = (TextView) findViewById(R.id.name);
 
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences result = getSharedPreferences("personal",0);
+
+                String userid = uid.getText().toString();
+                String userpwd = pwd.getText().toString();
+                String username = name.getText().toString();
+                result.edit()
+                        .putString("uid", userid)
+                        .putString("upwd", userpwd)
+                        .putString("uname", username)
+                        .commit();
+                finish();
+
 
             }
         });
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                uid.setText("");
+                pwd.setText("");
+                name.setText("");
             }
         });
 
     }
-
-//    public void register(View view){
-//        SharedPreferences result = getSharedPreferences("personal",0);
-//
-//        if (result.contains("uid")){
-//            if (result.contains("upwd")) {
-//                AlertDialog.Builder d = new AlertDialog.Builder(MainActivity.this);
-//                d.setTitle("警告!")
-//                        .setMessage("已註冊")
-//                        .setCancelable(false);
-//                d.setPositiveButton("確定", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-//
-//                    }
-//                });
-//                d.show();
-//            }
-//        }else {
-//            Intent it = new Intent(MainActivity.this, Main2Activity.class);
-//            startActivity(it);
-//        }
-//    }
-
 }
